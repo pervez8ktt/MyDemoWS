4a86fd5e49198f66ebe62c14b360b53a02b49f87
Pervez <pervez8ktt@gmail.com>
2017-10-10 00:10:19 +0530

    Create ws with two param

--- src/main/java/com/pervez/ws/controller/MainController.java
+++ src/main/java/com/pervez/ws/controller/MainController.java
@@ -3,14 +3,28 @@ package com.pervez.ws.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.pervez.ws.model.MyResponse;
 
 @Controller
 public class MainController {
 
-	@RequestMapping(value="/helloworld", method=RequestMethod.GET)
-	public @ResponseBody String helloWorld(){
-		return "hello World";
+	Gson gson = new Gson();
+	
+	@RequestMapping(value="/helloworld", method=RequestMethod.POST)
+	public @ResponseBody String helloWorld(@RequestParam String name, @RequestParam String lastname){
+		
+		MyResponse myResponse = new MyResponse();
+		myResponse.setName(name);
+		myResponse.setLastname(lastname);
+		
+		return gson.toJson(myResponse);
 	}
+	
+	
+	
 	
 }
---
+++ src/main/java/com/pervez/ws/model/MyResponse.java
@@ -0,0 +1,20 @@
+package com.pervez.ws.model;
+
+public class MyResponse {
+	
+	String name;
+	String lastname;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+}
